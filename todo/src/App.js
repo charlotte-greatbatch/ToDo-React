031652c21428ca870939b53245ad15db57a18880
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-08-11 12:10:30 +1200

    added state to TodoList with an onSubmit function

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ToDoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
     <div className="App">
-      <ToDoForm />
+      <TodoList />
     </div>
   )
 }
---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import ToDoForm from './TodoForm'
+
+function TodoList() {
+  const [todos, setTodos] = useState([])
+
+  const addToDo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return
+    }
+    const newTodos = [todo, ...todos]
+
+    setTodos(newTodos)
+  }
+  return (
+    <div>
+      <h1>What's the plan for today?</h1>
+      <ToDoForm onSubmit={addToDo} />
+    </div>
+  )
+}
+
+export default TodoList
